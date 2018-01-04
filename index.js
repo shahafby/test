5a2a2a96bf60f76f049c1273e55f3b8b7fb36816
shahafby <shahaf1991@gmail.com>
2018-01-04 08:49:31 +0200

    changed index.js

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
+const PORT = process.env.PORT || 5000
 const express = require('express');
-const bodyParser = require('body-parser');
 const fs = require('fs');
-
 let app = express();
-app.set('port', (process.env.PORT || 5000));
 
 app.use(bodyParser.json());
 
+
 app.get('/getFile/:filename',(req, res) => {
-    let fileToRead = req.params.filename || "showMyName.txt";
-    const src = fs.createReadStream(fileToRead);
+    let filename = req.params.filename || "test.txt";
+    const src = fs.createReadStream(filename);
     src.on('error', handler);
     src.pipe(res);
 });
-
-app.listen(app.get('port'), () => {
-    console.log(`Listening on port ${app.get('port')}!`);
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}!`);
 });
 
-// error-handling function (written as arrow function)
-const handler = err => console.log(err);
+const handler = err => console.error(err);
