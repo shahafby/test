5e9a4f3913e4bccf640da4589589c92c2a8b84c2
shahafby <shahaf1991@gmail.com>
2018-01-04 21:53:30 +0200

    fixed issues

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const bodyParser = require('body-parser');
 let app = express();
 
 app.use(bodyParser.json());
+app.set('port', (PORT));
 
+app.get('/getTime', (req, res) => {
+	let time = calcTime();
+	res.send(`The time now is: ${time}`);
+});
 
 app.get('/getFile/:filename',(req, res) => {
     let filename = req.params.filename || "test.txt";
@@ -17,5 +22,13 @@ app.get('/getFile/:filename',(req, res) => {
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}!`);
 });
+
+let calcTime = () => {
+	let date = new Date(),
+	hours = (date.getHours() < 10 ? '0' : '') + date.getHours(),
+	minutes = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
+	
+	return  (hours + ':' + minutes);
+}
 
 const handler = err => console.error(err);
